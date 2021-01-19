0ff546d6cba569951d7e26f28f5819ffa4b779df
Keepin-it-REAL <sj26080@naver.com>
2021-01-19 17:01:59 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,4 @@
+
+coordinates = [4, 5, 21]
+coordinates[1] = 10
+print(coordinates[2])
---
+++ functions.py
@@ -0,0 +1,8 @@
+
+def say_hi(name, age):
+    print("Hello " + name + ",you are " + str(age))
+
+say_hi("Mike", 25)
+say_hi("Steve", 70)
+say_hi("SungJoon", 65)
+say_hi("Yourim", 28)
